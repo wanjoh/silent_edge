8de48dc2a23bd367d7c5c79706a042ac5a259f1c
lukastan <lukastankovic170401@gmail.com>
2023-12-08 13:47:53 +0100

    Ovo i dalje ne radi.

--- src/map/map.cpp
+++ src/map/map.cpp
@@ -15,12 +15,15 @@ QGraphicsItemGroup* Map::draw_matrix(QString* path_to_map)
 {
     QGraphicsItemGroup *group = new QGraphicsItemGroup;
 
-    QFile file(*path_to_map);
-    if (file.open(QIODevice::ReadOnly))
+    QFile file("map_matrix.txt");
+    if (file.open(QIODevice::ReadOnly | QIODevice::Text))
     {
         QTextStream stream(&file);
         const int n = stream.read(1).toInt();
+        stream.read(1);
         const int m = stream.read(1).toInt();
+        stream.readLine();
+        qDebug() << "file opened";
 
         for (int i = 0; i < n; i++)
         {
@@ -43,6 +46,8 @@ QGraphicsItemGroup* Map::draw_matrix(QString* path_to_map)
                 EntityDrawer *tile = new EntityDrawer(path);
                 tile->setPos(i*IMAGE_SIZE, j*IMAGE_SIZE);
                 group->addToGroup(tile);
+
+                stream.read(1);
             }
         }
 
